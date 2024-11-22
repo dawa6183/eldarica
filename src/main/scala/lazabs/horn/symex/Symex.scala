229596bcd35291f45604e3f0db1b72cc11e82ed8
dawa6183 <dawa6183@student.uu.se>
2024-11-22 13:16:07 +0100

    Bug fix in hasContradiction which might be reached when using backward resolution. In the previous code "False :- P(..) /\ c" was considered to be a contradiction if c was satisfiable. It did not account for the predicate P in the body.

--- src/main/scala/lazabs/horn/symex/Symex.scala
+++ src/main/scala/lazabs/horn/symex/Symex.scala
@@ -322,7 +322,7 @@ abstract class Symex[CC](iClauses:    Iterable[CC])(
   // true if cuc = "FALSE :- c" and c is satisfiable, false otherwise.
   protected def hasContradiction(cuc:          UnitClause,
                                proverStatus: ProverStatus.Value): Boolean = {
-    ((cuc.rs.pred == HornClauses.FALSE) || (!cuc.isPositive)) &&
+    ((cuc.rs.pred == HornClauses.FALSE) || (!cuc.isPositive && cuc.rs.pred == HornClauses.FALSE)) &&
     (proverStatus match { // check if cuc constraint is satisfiable
       case ProverStatus.Valid | ProverStatus.Sat     => true
       case ProverStatus.Invalid | ProverStatus.Unsat => false
